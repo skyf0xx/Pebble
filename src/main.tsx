2496eeec7f467fbc06fdd0506b13bcccae246f64
Will <will@wander.com>
2026-05-28 21:54:56 +1000

    feat: implement session state manager

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { useAppStore } from './store'
+
+const wsUrl = new URLSearchParams(window.location.search).get('ws')
+useAppStore.getState().setWsUrl(wsUrl)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
